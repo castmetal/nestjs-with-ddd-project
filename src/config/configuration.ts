5b3f46460571a8003c0fb9cdb3f3bd998b729651
michel <michel.laguardia@sunoresearch.com.br>
2022-09-01 16:31:25 -0300

    chore: refactoring code

--- src/config/configuration.ts
+++ src/config/configuration.ts
@@ -1,23 +1,21 @@
-import 'dotenv/config';
-
 export default () => ({
   database: {
-    host: process.env.DATABASE_HOST ?? 'localhost',
-    port: parseInt(process.env.DATABASE_PORT, 10) ?? 5432,
-    username: process.env.DATABASE_USERNAME ?? 'myusername',
-    password: process.env.DATABASE_PASSWORD ?? 'mypassword',
-    db: process.env.DATABASE_DB ?? 'postgres',
-    schema: process.env.DATABASE_SCHEMA ?? 'public',
+    host: process.env.DATABASE_HOST || 'localhost',
+    port: parseInt(process.env.DATABASE_PORT, 10) || 5432,
+    username: process.env.DATABASE_USERNAME || 'myusername',
+    password: process.env.DATABASE_PASSWORD || 'mypassword',
+    db: process.env.DATABASE_DB || 'postgres',
+    schema: process.env.DATABASE_SCHEMA || 'public',
   },
   orm: {
     entities: __dirname + '/../core/domains/**/*.entity{.ts,.js}',
     migrations: __dirname + '/../infra/db/migrations/*{.ts,.js}',
   },
   security: {
-    salt: process.env.SALT_HASH ?? '12345678910ABMNJHGTSYUIOASDKKJASDaa;;.ças',
-    jwt_secret: process.env.JWT_SECRET ?? 'my.jwt.secret;',
+    salt: process.env.SALT_HASH || '12345678910ABMNJHGTSYUIOASDKKJASDaa;;.ças',
+    jwt_secret: process.env.JWT_SECRET || 'my.jwt.secret;',
   },
   authorization: {
-    header: process.env.AUTHORIZATION_HEADER ?? 'Authorization',
+    header: process.env.AUTHORIZATION_HEADER || 'Authorization',
   },
 });
